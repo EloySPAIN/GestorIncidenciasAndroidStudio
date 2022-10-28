1695a0ec303d4ad72cc94ae77f71ff59312be4fe
Jordi Ribellas <ribellas2002@gmail.com>
2022-10-28 16:48:04 +0200

    Intents de connectar a la BBD, pero no va

--- app/src/main/java/com/example/gestorincidencies/ConnexioBD.java
+++ app/src/main/java/com/example/gestorincidencies/ConnexioBD.java
@@ -1,20 +1,22 @@
 package com.example.gestorincidencies;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
-
+import android.widget.Toast;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class ConnexioBD {
+public class ConnexioBD{
 
     Connection conexio;
     String username, pass, ip, port, database;
 
+    @SuppressLint("NewApi")
     public Connection connect(){
-        ip="127.0.0.1";
+        ip="192.168.5.63";
         username="jordi";
         pass="1234";
-        port="1433";
+        port="3306";
         database="act2m8";
 
         //falta StrictMode
@@ -23,11 +25,12 @@ public class ConnexioBD {
         String connectionURL = null;
 
         try{
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            connectionURL="jdbc:jtds:sqlserver://"+ip+":"+port+";"+"databasename="+database+";user=" + username + ";password=" + pass + ";";
-            connection= DriverManager.getConnection(connectionURL);
+            Class.forName("com.mysql.jdbc.Driver");
+//          connectionURL="jdbc:mysql://"+ip+":"+port+";"+"databasename="+database+";user=" + username + ";password=" + pass + ";";
+            connectionURL="jdbc:mysql://192.168.5.63:3306/act2m8";
+            connection= DriverManager.getConnection(connectionURL, username, pass);
         }catch(Exception ex){
-            Log.e("Error: ", ex.getMessage());
+            Log.e("Error ", ex.getMessage());
         }
         return connection;
     }
--- app/src/main/java/com/example/gestorincidencies/LlistatIncidencies.java
+++ app/src/main/java/com/example/gestorincidencies/LlistatIncidencies.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 public class LlistatIncidencies extends AppCompatActivity {
     private Button btMostrar;
     private Button btn;
-    private TextView tvResultat;
+    private TextView tvIdResultat, tvNomResultat, tvCognomResultat, tvResumResultat, tvTelefResultat, tvDescResultat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +25,44 @@ public class LlistatIncidencies extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LlistatIncidencies.this,MainActivity.class));
+                startActivity(new Intent(LlistatIncidencies.this, MainActivity.class));
             }
         });
     }
 
     Connection connection;
-    String ConnectionResult="";
+    String ConnectionResult = "";
 
-    public void getTextFromSQL(View v){
+    public void getTextFromSQL(View v) {
         btMostrar = findViewById(R.id.mostrar);
-        tvResultat = findViewById(R.id.tvidResultat);
+        tvIdResultat = findViewById(R.id.tvidResultat);
+        tvNomResultat = findViewById(R.id.tvnomResultat);
+        tvCognomResultat = findViewById(R.id.tvcognomResultat);
+        tvResumResultat = findViewById(R.id.tvresumResultat);
+        tvTelefResultat = findViewById(R.id.tvtelefonResultat);
+        tvDescResultat = findViewById(R.id.tvdescripcioResultat);
 
-        try{
+        try {
             ConnexioBD connectionHelper = new ConnexioBD();
             connection = connectionHelper.connect();
-            if(connection!=null){
+            if (connection != null) {
                 String query = "Select * from incidencies";
                 Statement st = connection.createStatement();
                 ResultSet rs = st.executeQuery(query);
 
-                while(rs.next()){
-                    tvResultat.setText(rs.getString());
+                while (rs.next()) {
+                    tvIdResultat.setText(rs.getString(1));
+                    tvNomResultat.setText(rs.getString(2));
+                    tvCognomResultat.setText(rs.getString(3));
+                    tvResumResultat.setText(rs.getString(4));
+                    tvTelefResultat.setText(rs.getString(6));
+                    tvDescResultat.setText(rs.getString(5));
                 }
+            }else{
+                ConnectionResult="Check Connection";
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             Log.e("Error: ", ex.getMessage());
         }
     }
+}
