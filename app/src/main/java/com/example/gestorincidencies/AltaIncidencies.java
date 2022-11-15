c60f01110c6308382092c6d18ccf0baa88c45f13
EloySPAIN <yole.tres@gmail.com>
2022-11-15 16:40:30 +0100

    Alta incidencia pantalla

--- app/src/main/java/com/example/gestorincidencies/AltaIncidencies.java
+++ app/src/main/java/com/example/gestorincidencies/AltaIncidencies.java
@@ -3,12 +3,16 @@ package com.example.gestorincidencies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AltaIncidencies extends AppCompatActivity {
     private Button btn;
+    private Spinner spinnerMarca;
+    private ArrayAdapter<CharSequence>adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,11 @@ public class AltaIncidencies extends AppCompatActivity {
         setContentView(R.layout.alta);
 
         btn = findViewById(R.id.homeAlta);
+        spinnerMarca = findViewById(R.id.spinnerTipus);
+        adaptador=ArrayAdapter.createFromResource(this, R.array.tipusMarca, android.R.layout.simple_spinner_item);
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spinnerMarca.setAdapter(adaptador);
 
         btn.setOnClickListener(new View.OnClickListener() {
           @Override
--- app/src/main/java/com/example/gestorincidencies/LlistatIncidencies.java
+++ app/src/main/java/com/example/gestorincidencies/LlistatIncidencies.java
@@ -25,31 +25,32 @@ public class LlistatIncidencies extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LlistatIncidencies.this,MainActivity.class));
+                startActivity(new Intent(LlistatIncidencies.this, MainActivity.class));
             }
         });
     }
 
     Connection connection;
-    String ConnectionResult="";
+    String ConnectionResult = "";
 
-    public void getTextFromSQL(View v){
+    public void getTextFromSQL(View v) {
         btMostrar = findViewById(R.id.mostrar);
         tvResultat = findViewById(R.id.tvidResultat);
 
-        try{
+        try {
             ConnexioBD connectionHelper = new ConnexioBD();
             connection = connectionHelper.connect();
-            if(connection!=null){
+            if (connection != null) {
                 String query = "Select * from incidencies";
                 Statement st = connection.createStatement();
                 ResultSet rs = st.executeQuery(query);
 
-                while(rs.next()){
-                    tvResultat.setText(rs.getString());
+                while (rs.next()) {
+//                    tvResultat.setText(rs.getString());
                 }
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             Log.e("Error: ", ex.getMessage());
         }
     }
+}
