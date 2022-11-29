6f29672e84c42bfb6dc35ed68116444672c2c50e
Jordi Ribellas <ribellas2002@gmail.com>
2022-11-29 15:15:26 +0100

    documentació

--- app/src/main/java/com/example/gestorincidencies/ConnexioBD.java
+++ app/src/main/java/com/example/gestorincidencies/ConnexioBD.java
@@ -8,28 +8,35 @@ import java.sql.DriverManager;
 
 public class ConnexioBD{
 
+    //Aqui declarem les variables que fem servir
     Connection conexio;
     String username, pass, ip, port, database;
 
+    //Creem una funcio la qual criadrem des d'altres classes
     @SuppressLint("NewApi")
     public Connection connect(){
+        //declarem les variables amb els parametres com la ip, el port...
         ip="192.168.1.233";
         username="jordi";
         pass="123";
         port="3306";
         database="act2m8";
 
+        //declarem les variables de conexió a null
         Connection connection = null;
         String connectionURL = "";
 
+        //declararem el strict mode per a poder connectar-nos sense problema a la BBDD
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        //farem un try catch per a si no funciona el connector a la BBDD
         try{
             Class.forName("com.mysql.jdbc.Driver");
             connectionURL="jdbc:mysql://"+ip+":"+port+"/"+database+"?allowPublicKeyRetrieval=true&useSSL=false";
             connection= DriverManager.getConnection(connectionURL, username, pass);
         }catch(Exception ex){
+            //aqui ens mostrarà el missatge d'error en cas de que fallés
             Log.e("Error ", ex.getMessage());
         }
         return connection;
--- app/src/main/java/com/example/gestorincidencies/LlistatIncidencies.java
+++ app/src/main/java/com/example/gestorincidencies/LlistatIncidencies.java
@@ -17,31 +17,41 @@ import java.util.*;
 
 public class LlistatIncidencies extends AppCompatActivity {
 
+    //Aqui declarem les variables que fem servir
     private Button btn;
     Connection connect;
     private String id;
     private EditText idAMostrar;
 
+    //creem el metode onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.llistat);
+
+        //declarem les variables locals del onCreate
         btn = findViewById(R.id.home);
         idAMostrar = findViewById(R.id.idAMostrar);
         id = idAMostrar.getText().toString().trim();
 
         btn.setOnClickListener(new View.OnClickListener() {
+
+            //declarem la funcio onClick dins del onCreate aquesta funció la tindrà el botó de homr
             @Override
             public void onClick(View v) {
+                //Cridem al main activity
                 startActivity(new Intent(LlistatIncidencies.this, MainActivity.class));
             }
         });
     }
 
+    //declarem les variables necessaries per a la connexió a la BBDD
     Connection connection;
     String ConnectionResult = "";
 
+    //declarem el mètode que farà el select de la BBDD
     public void getTextFromSQL(View v) {
+        //devlarem les variables linkantles amb la part visual per a poder tractar-les
         TextView user = (TextView) findViewById(R.id.tvUser);
         TextView tipus = (TextView) findViewById(R.id.tvTipus);
         TextView marca = (TextView) findViewById(R.id.tvMarca);
@@ -49,15 +59,19 @@ public class LlistatIncidencies extends AppCompatActivity {
         TextView descripcio = (TextView) findViewById(R.id.tvDescripcio);
         TextView data = (TextView) findViewById(R.id.tvData);
 
+        //farem un try catch per a connectarnos a la BBDD i si falla ens mostrarà l'error
         try{
+            //cridem a la funcio de connectar-nos a la BBDD del ConnexióBD
             ConnexioBD connexioBD = new ConnexioBD();
             connect = connexioBD.connect();
 
+            //si la connexió no es null farem el select de la BBDD
             if (connect != null) {
                 String query = "select * from incidencies2 where id='" + id + "';";
                 Statement smt = connect.createStatement();
                 ResultSet rs = smt.executeQuery(query);
 
+                //mentres la query vagi tenint lnies les asignarem a les variables de la part frontal
                 while(rs.next()){
                     user.setText(rs.getString(2));
                     user.setText(rs.getString(3));
@@ -68,6 +82,7 @@ public class LlistatIncidencies extends AppCompatActivity {
             }
 
         }catch (Exception ex){
+            //mostrarà l'error en cas de que no es pugui connectar a la BBDD
             Log.e("Error: ", ex.getMessage());
         }
     }
